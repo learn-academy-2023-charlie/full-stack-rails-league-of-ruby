e306ba183257defe7d078e41fec2773171f41ebd
Rashaan-Richardson <rashaan.richardson@gmail.com>
2023-05-25 12:35:55 -0600

    story 2

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,6 +3,8 @@ class BlogController < ApplicationController
     def index
         @blog = Blog.all
     end
-
-
+    def show
+        @show = Blog.find([:id])
+    end 
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
 root 'blog#index'
-
+get '/blog/:id' => 'blog#show'
 
 end
